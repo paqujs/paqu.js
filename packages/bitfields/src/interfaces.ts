32ffd2cd2ea55d3b5f750e0e3f5da1bf3c239c12
hanzydev <emirhanbaltas34@gmail.com>
2023-04-27 01:15:41 +0300

    fix(bitfields): fix typings

--- packages/bitfields/src/interfaces.ts
+++ packages/bitfields/src/interfaces.ts
@@ -11,22 +11,30 @@ import {
 } from './index';
 import type { Arrayable } from '@paqujs/shared';
 
-export type ApplicationFlagsBitsResolvable = Arrayable<keyof typeof ApplicationFlags | number>;
+export type ApplicationFlagsBitsResolvable = Arrayable<
+    keyof typeof ApplicationFlags | ApplicationFlags
+>;
 
-export type ChannelFlagsBitsResolvable = Arrayable<keyof typeof ChannelFlags | number>;
+export type ChannelFlagsBitsResolvable = Arrayable<keyof typeof ChannelFlags | ChannelFlags>;
 
-export type GuildMemberFlagsBitsResolvable = Arrayable<keyof typeof GuildMemberFlags | number>;
+export type GuildMemberFlagsBitsResolvable = Arrayable<
+    keyof typeof GuildMemberFlags | GuildMemberFlags
+>;
 
-export type MessageFlagsBitsResolvable = Arrayable<keyof typeof MessageFlags | number>;
+export type MessageFlagsBitsResolvable = Arrayable<keyof typeof MessageFlags | MessageFlags>;
 
 export type PermissionFlagsBitsResolvable = Arrayable<keyof typeof PermissionFlagsBits | number>;
 
-export type PresenceActivityFlagsBitsResolvable = Arrayable<keyof typeof ActivityFlags | number>;
-
-export type ThreadMemberFlagsBitsResolvable = Arrayable<keyof typeof ThreadMemberFlags | number>;
-
-export type SystemChannelFlagsBitsResolvable = Arrayable<
-    keyof typeof GuildSystemChannelFlags | number
+export type PresenceActivityFlagsBitsResolvable = Arrayable<
+    keyof typeof ActivityFlags | ActivityFlags
 >;
 
-export type UserFlagsBitsResolvable = Arrayable<keyof typeof UserFlags | number>;
+export type ThreadMemberFlagsBitsResolvable = Arrayable<
+    keyof typeof ThreadMemberFlags | ThreadMemberFlags
+>;
+
+export type SystemChannelFlagsBitsResolvable = Arrayable<
+    keyof typeof GuildSystemChannelFlags | GuildSystemChannelFlags
+>;
+
+export type UserFlagsBitsResolvable = Arrayable<keyof typeof UserFlags | UserFlags>;
