7f19436426fdaa64146a9984d622889762a28976
hanzydev <hanzydev1@gmail.com>
2023-07-11 00:24:31 +0300

    chore: release shared@1.1.7

--- packages/shared/src/structures/TypedEmitter.ts
+++ packages/shared/src/structures/TypedEmitter.ts
@@ -10,55 +10,55 @@ export type ListenerSignature<L> = {
 };
 
 export declare interface TypedEmitter<L extends ListenerSignature<L> = DefaultListener> {
-    addListener<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<void>): this;
+    addListener<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<any>): this;
     addListener<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => Awaitable<void>,
+        listener: (...args: any[]) => Awaitable<any>,
     ): this;
 
     prependListener<E extends keyof L>(
         event: E,
-        listener: (...args: L[E]) => Awaitable<void>,
+        listener: (...args: L[E]) => Awaitable<any>,
     ): this;
     prependListener<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => Awaitable<void>,
+        listener: (...args: any[]) => Awaitable<any>,
     ): this;
 
     prependOnceListener<E extends keyof L>(
         event: E,
-        listener: (...args: L[E]) => Awaitable<void>,
+        listener: (...args: L[E]) => Awaitable<any>,
     ): this;
     prependOnceListener<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => Awaitable<void>,
+        listener: (...args: any[]) => Awaitable<any>,
     ): this;
 
-    removeListener<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<void>): this;
+    removeListener<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<any>): this;
     removeListener<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => Awaitable<void>,
+        listener: (...args: any[]) => Awaitable<any>,
     ): this;
 
     removeAllListeners<E extends keyof L>(event?: E): this;
     removeAllListeners<S extends string | symbol>(event?: Exclude<S, keyof L>): this;
 
-    once<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<void>): this;
+    once<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<any>): this;
     once<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => Awaitable<void>,
+        listener: (...args: any[]) => Awaitable<any>,
     ): this;
 
-    on<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<void>): this;
+    on<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<any>): this;
     on<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => Awaitable<void>,
+        listener: (...args: any[]) => Awaitable<any>,
     ): this;
 
-    off<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<void>): this;
+    off<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<any>): this;
     off<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => Awaitable<void>,
+        listener: (...args: any[]) => Awaitable<any>,
     ): this;
 
     emit<E extends keyof L>(event: E, ...args: L[E]): boolean;
@@ -73,12 +73,12 @@ export declare interface TypedEmitter<L extends ListenerSignature<L> = DefaultLi
     listeners<E extends keyof L>(event: E): L[E][];
     listeners<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-    ): ((...args: any[]) => Awaitable<void>)[];
+    ): ((...args: any[]) => Awaitable<any>)[];
 
     rawListeners<E extends keyof L>(event: E): L[E][];
     rawListeners<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-    ): ((...args: any[]) => Awaitable<void>)[];
+    ): ((...args: any[]) => Awaitable<any>)[];
 }
 
 export class TypedEmitter extends EventEmitter {
