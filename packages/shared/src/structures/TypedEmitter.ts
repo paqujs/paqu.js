6f9118bc80080300fcda26a6259f40c378243fc0
hanzydev <hanzydev1@gmail.com>
2023-07-11 00:16:18 +0300

    chore: release shared@1.1.6

--- packages/shared/src/structures/TypedEmitter.ts
+++ packages/shared/src/structures/TypedEmitter.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from 'events';
+import type { Awaitable } from '../index';
 
 export type DefaultListener = {
     [event: string]: any[];
@@ -9,49 +10,55 @@ export type ListenerSignature<L> = {
 };
 
 export declare interface TypedEmitter<L extends ListenerSignature<L> = DefaultListener> {
-    addListener<E extends keyof L>(event: E, listener: (...args: L[E]) => any): this;
+    addListener<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<void>): this;
     addListener<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => void,
+        listener: (...args: any[]) => Awaitable<void>,
     ): this;
 
-    prependListener<E extends keyof L>(event: E, listener: (...args: L[E]) => any): this;
+    prependListener<E extends keyof L>(
+        event: E,
+        listener: (...args: L[E]) => Awaitable<void>,
+    ): this;
     prependListener<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => void,
+        listener: (...args: any[]) => Awaitable<void>,
     ): this;
 
-    prependOnceListener<E extends keyof L>(event: E, listener: (...args: L[E]) => any): this;
+    prependOnceListener<E extends keyof L>(
+        event: E,
+        listener: (...args: L[E]) => Awaitable<void>,
+    ): this;
     prependOnceListener<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => void,
+        listener: (...args: any[]) => Awaitable<void>,
     ): this;
 
-    removeListener<E extends keyof L>(event: E, listener: (...args: L[E]) => any): this;
+    removeListener<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<void>): this;
     removeListener<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => void,
+        listener: (...args: any[]) => Awaitable<void>,
     ): this;
 
     removeAllListeners<E extends keyof L>(event?: E): this;
     removeAllListeners<S extends string | symbol>(event?: Exclude<S, keyof L>): this;
 
-    once<E extends keyof L>(event: E, listener: (...args: L[E]) => any): this;
+    once<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<void>): this;
     once<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => void,
+        listener: (...args: any[]) => Awaitable<void>,
     ): this;
 
-    on<E extends keyof L>(event: E, listener: (...args: L[E]) => any): this;
+    on<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<void>): this;
     on<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => void,
+        listener: (...args: any[]) => Awaitable<void>,
     ): this;
 
-    off<E extends keyof L>(event: E, listener: (...args: L[E]) => any): this;
+    off<E extends keyof L>(event: E, listener: (...args: L[E]) => Awaitable<void>): this;
     off<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-        listener: (...args: any[]) => void,
+        listener: (...args: any[]) => Awaitable<void>,
     ): this;
 
     emit<E extends keyof L>(event: E, ...args: L[E]): boolean;
@@ -64,12 +71,14 @@ export declare interface TypedEmitter<L extends ListenerSignature<L> = DefaultLi
     listenerCount<S extends string | symbol>(type: Exclude<S, keyof L>): number;
 
     listeners<E extends keyof L>(event: E): L[E][];
-    listeners<S extends string | symbol>(event: Exclude<S, keyof L>): ((...args: any[]) => void)[];
+    listeners<S extends string | symbol>(
+        event: Exclude<S, keyof L>,
+    ): ((...args: any[]) => Awaitable<void>)[];
 
     rawListeners<E extends keyof L>(event: E): L[E][];
     rawListeners<S extends string | symbol>(
         event: Exclude<S, keyof L>,
-    ): ((...args: any[]) => void)[];
+    ): ((...args: any[]) => Awaitable<void>)[];
 }
 
 export class TypedEmitter extends EventEmitter {
