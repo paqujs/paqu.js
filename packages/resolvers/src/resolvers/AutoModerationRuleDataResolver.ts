fef5feccccd86d43a3520b1c06158fcee545b2c8
hanzydev <hanzydev1@gmail.com>
2023-07-16 18:33:47 +0300

    feat(resolvers): add missing props

--- packages/resolvers/src/resolvers/AutoModerationRuleDataResolver.ts
+++ packages/resolvers/src/resolvers/AutoModerationRuleDataResolver.ts
@@ -1,10 +1,14 @@
-import { AutoModerationRuleEventType } from 'discord-api-types/v10';
+import { AutoModerationRuleEventType, AutoModerationRuleTriggerType } from 'discord-api-types/v10';
 
 export function AutoModerationRuleDataResolver(rule: any) {
     const res = rule;
 
     if (res.event_type && typeof res.event_type !== 'number') {
         res.event_type = AutoModerationRuleEventType[res.event_type];
+    }
+
+    if (res.trigger_type && typeof res.trigger_type !== 'number') {
+        res.trigger_type = AutoModerationRuleTriggerType[res.trigger_type];
     }
 
     return res;
